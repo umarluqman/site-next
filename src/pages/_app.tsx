e819a3afce6a13fc9ed2a0139eeff43a966b3f7b
Umar Luqman <>
2021-06-06 09:21:07 +0800

    Init ts and chakra ui

---
+++ src/pages/_app.tsx
@@ -0,0 +1,15 @@
+import "../styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
+
+import theme from "../theme";
+import { AppProps } from "next/app";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider resetCSS theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
+}
+
+export default MyApp;
