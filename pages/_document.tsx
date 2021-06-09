d89a7ea9131496ebeb22bee3874f08c074bea948
Umar Luqman <>
2021-06-09 22:44:02 +0800

    Add favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,6 @@
 import NextDocument, { Html, Head, Main, NextScript } from "next/document";
 import { ColorModeScript } from "@chakra-ui/react";
+import { render } from "nprogress";
 
 export default class Document extends NextDocument {
   render() {
@@ -7,6 +8,25 @@ export default class Document extends NextDocument {
       <Html>
         <Head>
           <link rel="stylesheet" type="text/css" href="/nprogress.css" />
+          <link href="/favicon/favicon.ico" rel="shortcut icon" />
+          <link href="/favicon/site.webmanifest" rel="manifest" />
+          <link
+            href="/favicon/apple-touch-icon.png"
+            rel="apple-touch-icon"
+            sizes="180x180"
+          />
+          <link
+            href="/favicon/favicon-32x32.png"
+            rel="icon"
+            sizes="32x32"
+            type="image/png"
+          />
+          <link
+            href="/favicon/favicon-16x16.png"
+            rel="icon"
+            sizes="16x16"
+            type="image/png"
+          />
         </Head>
 
         <body>
