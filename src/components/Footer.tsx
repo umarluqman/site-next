bfff94cbad05366cb3964bea4e708ad93f28d33c
Umar Luqman <>
2021-06-15 10:34:31 +0800

    Add footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,26 @@
-import { chakra } from "@chakra-ui/react";
+import { chakra, Text } from "@chakra-ui/react";
+import { Github, Twitter } from "emotion-icons/feather";
 import React from "react";
 
-const Footer = () => {
-  return <footer></footer>;
-};
+export const Footer = () => {
+  return (
+    <chakra.footer
+      display="flex"
+      p={8}
+      width="full"
+      justifyContent="flex-end"
+      alignItems="center"
+      mt={12}
+      maxW={800}
+      w="full"
+      m="0 auto"
+    >
+      <Text>Made with love from 🇲🇾</Text>
+      <chakra.div w={3}></chakra.div>
+      <Github width={24} strokeWidth={1.8}></Github>
+      <chakra.div w={3}></chakra.div>
 
-export default Footer;
+      <Twitter width={24} strokeWidth={1.8}></Twitter>
+    </chakra.footer>
+  );
+};
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,6 @@
 import { chakra, Grid, useColorModeValue } from "@chakra-ui/react";
 import React, { ReactNode } from "react";
-import Footer from "./Footer";
+import { Footer } from "./Footer";
 import { Header } from "./Header";
 
 export const Layout = ({ children }: { children: ReactNode }) => {
@@ -8,7 +8,7 @@ export const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <Grid templateRows="auto 1fr auto" bg={bg}>
       <Header />
-      <chakra.main minHeight={"88vh"}>{children}</chakra.main>
+      <chakra.main minHeight={"82vh"}>{children}</chakra.main>
       <Footer />
     </Grid>
   );
