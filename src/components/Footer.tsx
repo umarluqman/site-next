4f1753b50bc6562375f684638415e1472efc6f9d
Umar Luqman <>
2021-06-20 13:02:05 +0800

    Added profile social media links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
 import { chakra, Text, Link } from "@chakra-ui/react";
-import { Github, Twitter } from "emotion-icons/feather";
+import { Codesandbox, Github, Linkedin, Twitter } from "emotion-icons/feather";
 import React from "react";
 
 export const Footer = () => {
@@ -7,24 +7,42 @@ export const Footer = () => {
     <chakra.footer
       display="flex"
       p={8}
+      pt={{ base: 20, sm: 8 }}
       width="full"
-      justifyContent="flex-end"
+      flexDir={{ base: "column", md: "row" }}
+      justifyContent={{ md: "flex-end" }}
       alignItems="center"
-      mt={12}
       maxW={800}
       w="full"
       m="0 auto"
     >
-      <Text>Made with love from 🇲🇾</Text>
-      <chakra.div w={3}></chakra.div>
-      <Link href="https://github.com" isExternal>
-        <Github width={24} strokeWidth={1.8}></Github>
-      </Link>
-      <chakra.div w={3}></chakra.div>
-
-      <Link href="https://twitter.com/umarlqmn" isExternal>
-        <Twitter width={24} strokeWidth={1.8}></Twitter>
-      </Link>
+      <chakra.ul
+        display={{ base: "flex", lg: "none" }}
+        listStyleType="none"
+        w="full"
+        justifyContent={{ base: "space-between", sm: "flex-start" }}
+      >
+        <Link href="https://www.linkedin.com/in/umarluqman/" isExternal>
+          <chakra.li mr={2} ml={4}>
+            <Linkedin width={24} strokeWidth={1.8}></Linkedin>
+          </chakra.li>
+        </Link>
+        <Link href="https://codesandbox.io/u/umarluqman" isExternal>
+          <chakra.li mx={2}>
+            <Codesandbox width={24} strokeWidth={1.8}></Codesandbox>
+          </chakra.li>
+        </Link>
+        <Link href="https://github.com" isExternal>
+          <chakra.li mx={2}>
+            <Github width={24} strokeWidth={1.8}></Github>
+          </chakra.li>
+        </Link>
+        <Link href="https://twitter.com/umarlqmn" isExternal>
+          <chakra.li mx={2}>
+            <Twitter width={24} strokeWidth={1.8}></Twitter>
+          </chakra.li>
+        </Link>
+      </chakra.ul>
     </chakra.footer>
   );
 };
